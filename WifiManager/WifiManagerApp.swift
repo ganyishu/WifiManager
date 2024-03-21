dac8b8bea91f83d0185e3b661e4e0bce08268fb6
ganesh kumar <ganeshgany96@gmail.com>
2024-03-21 18:52:29 +0530

    Initial Commit

---
+++ WifiManager/WifiManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WifiManagerApp.swift
+//  WifiManager
+//
+//  Created by Gany Ganesh on 21/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WifiManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
